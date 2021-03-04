5a5ff52a4e98505a2d2ed8106671ecfafa9f076e
Jenny Kim <kimsujin128@naver.com>
2021-03-04 12:38:27 +0900

    first commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  paragraphTwo:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  buttonLabel: 'View Homes',
+  image:ImageOne,
+  reverse:false,
+  delay:100
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne:'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  paragraphTwo:'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  buttonLabel: 'View Homes',
+  image:ImageTwo,
+  reverse:true,
+  delay:300
+};
